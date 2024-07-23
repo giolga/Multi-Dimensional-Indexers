851e20ff82f72b7a55228e930ffa5667d17fc296
giolga <giolga.kumi@gmail.com>
2024-07-23 22:55:53 +0400

    first commit

---
+++ multiDimensionalIndexes/MDI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiDimensionalIndexes
+{
+    internal class MDI
+    {
+        private int[,] arr = new int[5,5];
+
+        public int this[int row, int col]
+        {
+            get
+            {
+                return arr[row,col];
+            }
+            set 
+            {
+                arr[row,col] = value;
+            }
+        }
+    }
+}
---
+++ multiDimensionalIndexes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiDimensionalIndexes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MDI mdi = new MDI();
+            Random random = new Random();
+
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    mdi[i, j] = random.Next(1, 100);
+                }
+            }
+
+            Console.WriteLine("The multidimensional indexers give us the following : ");
+            for (int i = 0;i < 4; i++) {
+                for(int j = 0; j < 4; j++)
+                    Console.Write(mdi[i,j] + " ");
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
